f235dfe928af69d2acb2017bc0099f9e02e9a97a
Ryan Higgins <c16381886@mydit.ie>
2017-12-04 13:46:24 +0000

    Things added

---
+++ Game Tools Game 4/Assets/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+	public bool test;
+	public string sceneToLoad;
+
+	void Start () {
+		test = false;
+	}
+
+	void OnTriggerEnter(Collider agent) {
+		test = true;
+		if (agent.tag == "Player") {
+			SceneManager.LoadScene (sceneToLoad, LoadSceneMode.Single);
+		}
+	}
+
+	void Update () {
+
+	}
+}
