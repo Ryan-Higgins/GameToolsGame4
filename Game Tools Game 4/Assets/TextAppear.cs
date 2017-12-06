d6cef3b906f1709e516c3f33db97a2275414ef3d
Ryan Higgins <c16381886@mydit.ie>
2017-12-06 23:25:20 +0000

    Tutorial done.

---
+++ Game Tools Game 4/Assets/TextAppear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAppear : MonoBehaviour {
+
+	public List<GameObject> texts;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider text) {
+		
+		texts[0].SetActive(true);
+			texts[1].SetActive (true);
+
+	}
+}
